cc842e773111c72435dc3311fb9fb628505e4dba
James Cheng <jamestzungcheng11@gmail.com>
2025-10-19 12:53:41 -0300

    Adicionar conteúdo do subprojeto PaisesEstadosCidades

---
+++ PaisesEstadosCidades/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaisesEstadosCidades
+{
+    internal class Controller
+    {
+
+
+
+        public Controller()
+        {
+
+        }
+            
+
+        public virtual void Excluir(Object obj)
+        {
+
+        }
+        public virtual string  Salvar(object obj)
+        {
+            return null;
+            
+        }
+
+        public virtual List<T> Listar<T>()
+        {
+            return null;
+
+
+        }
+        public virtual Object CarregaObj(int chave)
+        {
+            return null;
+        }
+
+        public virtual List<T>Pesquisar<T>(string chave)
+        {
+            return null;
+
+
+        }
+
+
+
+    }
+}
---
+++ PaisesEstadosCidades/CtrlPaises.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaisesEstadosCidades
+{
+    internal class CtrlPaises:Controller
+    {
+        ColecoesPaises acolpais;
+        DaoPaises aDaoPaises;
+
+
+        public CtrlPaises()
+        {
+            acolpais = new ColecoesPaises();
+            aDaoPaises = new DaoPaises();
+
+        }
+
+        public override string  Salvar(object obj)
+        {
+            return aDaoPaises.Salvar(obj);
+            
+            
+              
+             
+
+        }
+    }
+}
